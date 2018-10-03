372c347b02dd7f479fd5b64b0dd93b0617a4f6ff
TinyChicken12 <Tnkien12vcu@gmail.com>
2018-10-03 14:09:43 +0700

    add files.

---
+++ src/MethodPalindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MethodPalindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input string: ");
+        String s = scanner.next();
+
+        if (isPalindrome(s))
+            System.out.print("String is Palindrome!");
+        else System.out.print("String not Palindrome!");
+
+    }
+
+    public static boolean isPalindrome(String s) {
+        int length = s.length();
+        int i = 0;
+        boolean isLeng = (length == 0 || length == 1);
+
+        if (isLeng) {
+            return true;
+        } else {
+            if (s.charAt(i) == s.charAt(length - 1)) {
+                i++;
+                return isPalindrome(s.substring(i, length - 1));
+            } else return false;
+        }
+    }
+}
